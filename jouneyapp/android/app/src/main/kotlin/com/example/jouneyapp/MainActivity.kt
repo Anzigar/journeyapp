764588a7a3c2d299ca1f43e5dc249fc8a023bc7b
anzigar <ansigar@gmail.com>
2022-09-29 09:08:52 +0300

    add journey app

---
+++ jouneyapp/android/app/src/main/kotlin/com/example/jouneyapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jouneyapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ jouneyapp/lib/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _HomeScreen extends StatefulWidget {
+  const _HomeScreen({super.key});
+
+  @override
+  State<_HomeScreen> createState() => __HomeScreenState();
+}
+
+class __HomeScreenState extends State<_HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
